7d4a50b436e7e2a24e370c1335d612d6897e3fc1
VD <vudao1988@gmail.com>
2021-08-28 23:34:38 -0400

    Fix silly mistake DLL_THREAD_ATTACH

--- WorldEditor/dllmain.cpp
+++ WorldEditor/dllmain.cpp
@@ -13,9 +13,9 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
         DllInit();
         break;
+    case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
         break;
